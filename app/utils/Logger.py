039c9340bf960aea079576c170f4f56ba521496f
JoelTapia99 <joelts159@gmail.com>
2024-12-02 22:21:30 -0500

    add logger, separate db connection and update requirements.txt

--- app/utils/Logger.py
+++ app/utils/Logger.py
@@ -3,10 +3,10 @@ import os
 import traceback
 
 
-class Logger:
+class Logger():
 
     def __set_logger(self):
-        log_directory = 'src/utils/log'
+        log_directory = 'app/utils/log'
         log_filename = 'app.log'
 
         logger = logging.getLogger(__name__)
@@ -19,7 +19,7 @@ class Logger:
         formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s', "%Y-%m-%d %H:%M:%S")
         file_handler.setFormatter(formatter)
 
-        if logger.hasHandlers():
+        if (logger.hasHandlers()):
             logger.handlers.clear()
 
         logger.addHandler(file_handler)
@@ -31,15 +31,15 @@ class Logger:
         try:
             logger = cls.__set_logger(cls)
 
-            if level == "critical":
+            if (level == "critical"):
                 logger.critical(message)
-            elif level == "debug":
+            elif (level == "debug"):
                 logger.debug(message)
-            elif level == "error":
+            elif (level == "error"):
                 logger.error(message)
-            elif level == "info":
+            elif (level == "info"):
                 logger.info(message)
-            elif level == "warn":
+            elif (level == "warn"):
                 logger.warn(message)
         except Exception as ex:
             print(traceback.format_exc())
